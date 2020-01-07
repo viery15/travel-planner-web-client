4eeedb2fee218f7b694fa6a0430488c5c095adbb
Viery Darmawan <vierydarmawan2@gmail.com>
2020-01-07 16:23:15 +0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import 'bootstrap'
+import JQuery from 'jquery'
+import 'popper.js'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { faPen } from '@fortawesome/free-solid-svg-icons'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Loading from 'vue-loading-overlay';
+import { ClientTable } from 'vue-tables-2';
+
+window.$ = JQuery
+Vue.use(Loading);
+Vue.use(VueSweetalert2)
+Vue.use(ClientTable);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(faCoffee, faTrash, faEye, faPen, faPlus, faWindowClose)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <br><br>
+    <div class="row">
+      <div class="col-md-6">
+        <form action="/action_page.php">
+          <div class="form-group">
+            <label for="coordinate">Coordinate:</label><br>
+            <input type="text" class="form-control" v-model="latitude" style="width:40%; float:left; margin-right:2%">
+            <input type="text" class="form-control" v-model="longitude" style="width:40%; float:left;"><br>
+          </div>
+          <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" id="pwd">
+          </div>
+          <button type="button" class="btn btn-primary" disabled>Submit</button>
+        </form>
+      </div>
+      
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      latitude: "",
+      longitude: "",
+    }
   }
 }
 </script>
